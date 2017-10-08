9b058facc84797fa5d0f294d2eb6a91e54817c78
cgreenhill <cgreenhill@users.noreply.github.com>
2017-10-08 17:59:19 -0400

    Update Sequencer.cs

--- Assets/Scripts/Sequencer.cs
+++ Assets/Scripts/Sequencer.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class Sequencer : MonoBehaviour
@@ -9,15 +9,25 @@ public class Sequencer : MonoBehaviour
 	public AudioSource audio;
 	public bool visible;
 	public bool audible;
+	public bool sequencerActivated = false;
 	// Use this for initialization
 	void Start () 
 	{
 		visible = true;
 		audible = true;
+
 	}
 	// Update is called once per frame
 	void Update () 
 	{
+		foreach(var cube in cubes) {
+			var controller = cube.GetComponent<CubeController> ();
+			if (controller.cubeActivated){
+				sequencerActivated = true;
+				return;
+			}
+		}
+		sequencerActivated = false;
 	}
 
 }
