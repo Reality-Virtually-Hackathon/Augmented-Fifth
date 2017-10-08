624303b206207df376e58fce93c9d00eedbeb55c
Emily <emilysa@mit.edu>
2017-10-08 09:35:49 -0400

    Added character animation script

---
+++ Assets/Scripts/SoundCharacter.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundCharacter : MonoBehaviour {
+
+    Animator anim;
+
+    protected float DefaultAnimationSpeed = 1f;
+
+    // Use this for initialization
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+        SetAnimationSpeed(DefaultAnimationSpeed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // temp for testing
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Play();
+        }
+    }
+
+    public void SetAnimationSpeed(float value)
+    {
+        anim.SetFloat("Speed", value);
+    }
+
+    public void Play()
+    {
+        // start the play animation
+        anim.SetTrigger("Play");
+    }
+}
